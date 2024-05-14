f8a0a3e22097dea1e87b7b5e5ff069a89fcd2211
J-Bills <billsjamal@gmail.com>
2024-05-14 16:50:50 -0700

    sunset sunrise finder finish

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 import requests
+import datetime
 
-response = requests.get(url='http://api.open-notify.org/iss-now.json')
+MY_LAT = 0
+MY_LNG = 0
+
+# response = requests.get(url='http://api.open-notify.org/iss-now.json')
+# response.raise_for_status()
+
+# data=response.json()
+
+# longitude = data.get('iss_position')['longitude']
+# latitude = data.get('iss_position')['latitude']
+
+# iss_position = (longitude, latitude)
+
+# print(iss_position)
+
+paramaters = {
+    'lat': MY_LAT,
+    'lng': MY_LNG,
+    'formatted':0    
+}
+
+response = requests.get('https://api.sunrise-sunset.org/json', params=paramaters)
 response.raise_for_status()
 
 data=response.json()
+sunrise = data['results']['sunrise']
+sunset = data['results']['sunset']
 
-longitude = data.get('iss_position')['longitude']
-latitude = data.get('iss_position')['latitude']
+stuff = (sunrise,sunset)
 
-iss_position = (longitude, latitude)
-
-print(iss_position)
+time_now = datetime.datetime.now()
+print(stuff)
+print(time_now)
